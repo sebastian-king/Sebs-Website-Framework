d31c4cb21ffc16f92c56f3f4581b08c6fe2c431a
root <root@x.massivesoft.net>
2017-10-17 17:24:17 +0200

    added cron to installer

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 # framework installation script -- a work in progress
 
-mysql -e 'CREATE TABLE sent_emails (`id` int PRIMARY KEY NOT NULL AUTO_INCREMENT, `to` varchar(255), `subject` varchar(255), `message` TEXT, `headers` varchar(1000), `status` int(1));'
+mysql DATABASE_NAME < sql/*
+
+useradd sebs_web_framework_user
+passwd sebs_web_framework_user
+
+`cat tools/cron/crontab` > /etc/cron.d/sebs_web_framework
