9ab9195e4e246f0b7425152ac331b4b3545c4e05
Sebastian King <sebastian-king@users.noreply.github.com>
2018-12-19 03:50:08 -0600

    Update header.php

--- template/header.php
+++ template/header.php
@@ -1,18 +1,3 @@
 <?php
-
-$title = htmlspecialchars($title ? $title . " | " . WEBSITE_NAME : WEBSITE_NAME);
-
-$breadcrumbs = array();
-$i = 0;
-foreach ($breadcrumbs as $key => $val) {
-	if (++$i === count($breadcrumbs)) {
-		$breadcrumbs[$i] = array($val, $key, false);
-	} else {
-		$breadcrumbs[$i] = array($val, $key, true);
-	}
-}
-
-if ($heading == true && gettype($heading) == "boolean") {
-	$heading = $title;
-}
+// use $title, #breadcrumbs & $heading here
 ?>
